b35bee755a03e8c2e0f6a0e6eb61611ef88ff474
Antony Duran <antony@lambdacurry.dev>
2024-10-16 21:42:36 -0600

    feat: add product category heading

--- apps/storefront/app/components/sections/PageHeading.tsx
+++ apps/storefront/app/components/sections/PageHeading.tsx
@@ -4,5 +4,5 @@ import clsx from 'clsx';
 export interface PageHeadingProps extends HTMLAttributes<HTMLHeadingElement> {}
 
 export const PageHeading: FC<PageHeadingProps> = ({ className, ...props }) => (
-  <h1 className={clsx('max-w-full break-words text-4xl font-extrabold md:text-6xl', className)} {...props} />
+  <h1 className={clsx('max-w-full break-words text-4xl font-bold md:text-6xl font-ballet', className)} {...props} />
 );
--- apps/storefront/app/routes/categories.$categoryHandle.tsx
+++ apps/storefront/app/routes/categories.$categoryHandle.tsx
@@ -1,18 +1,20 @@
-import HomeIcon from '@heroicons/react/24/solid/HomeIcon';
 import { LoaderFunctionArgs, redirect } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { NavLink, useLoaderData } from '@remix-run/react';
 import { ProductListWithPagination } from '@app/components/product/ProductListWithPagination';
 import { sdk } from '@libs/util/server/client.server';
 import { Container } from '@app/components/common/container';
-import { Breadcrumbs } from '@app/components/common/breadcrumbs';
 import { getSelectedRegion } from '@libs/util/server/data/regions.server';
-import { getCategoryByHandle } from '@libs/util/server/data/categories.server';
+import { listCategories } from '@libs/util/server/data/categories.server';
+import { PageHeading } from '@app/components/sections/PageHeading';
+import clsx from 'clsx';
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const region = await getSelectedRegion(request.headers);
   const handle = params.categoryHandle as string;
 
-  const category = await getCategoryByHandle([handle]);
+  const categories = await listCategories();
+
+  const category = categories.find((c) => c.handle === handle);
 
   if (!category) {
     throw redirect('/products');
@@ -23,7 +25,14 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     category_id: category.id,
   });
 
-  return { products, count, limit, offset, category };
+  return {
+    products,
+    count,
+    limit,
+    offset,
+    category,
+    categories,
+  };
 };
 
 export type ProductCategoryRouteLoader = typeof loader;
@@ -33,32 +42,36 @@ export default function ProductCategoryRoute() {
 
   if (!data) return null;
 
-  const { products, count, limit, offset } = data;
-
-  const breadcrumbs = [
-    {
-      label: (
-        <span className="flex whitespace-nowrap">
-          <HomeIcon className="inline h-4 w-4" />
-          <span className="sr-only">Home</span>
-        </span>
-      ),
-      url: `/`,
-    },
-    {
-      label: 'All Products',
-      url: '/products',
-    },
-    {
-      label: data.category.name,
-    },
-  ];
+  const { products, count, limit, offset, categories } = data;
 
   return (
     <Container className="pb-16">
-      <div className="my-8 flex flex-wrap items-center justify-between gap-4">
-        <Breadcrumbs breadcrumbs={breadcrumbs} />
-      </div>
+      {categories.length > 1 && (
+        <div className="flex flex-col w-full items-center">
+          <div className="flex-1">
+            <div className="inline-flex gap-5 text-4xl font-italiana border-b border-primary my-5">
+              {categories.map((category) => (
+                <NavLink
+                  to={`/categories/${category.handle}`}
+                  key={category.id}
+                  className={({ isActive }) =>
+                    clsx('h-full p-4', {
+                      'font-bold border-b-2 border-primary': isActive,
+                      '!border-none active:': !isActive,
+                    })
+                  }
+                >
+                  {category.name}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+
+      <PageHeading className="w-full text-center text-5xl xs:text-6xl md:text-8xl font-ballet my-24 font-normal lg:font-normal">
+        {data.category.name}
+      </PageHeading>
 
       <div className="flex flex-col gap-4 sm:flex-row">
         <div className="flex-1">
