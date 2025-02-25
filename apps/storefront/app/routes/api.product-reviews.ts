8172e177e5265a266deea822c95ee58916a0a737
Antony Duran <antony@lambdacurry.dev>
2025-02-25 09:08:24 -0600

    product reviews test upload images

--- apps/storefront/app/routes/api.product-reviews.ts
+++ apps/storefront/app/routes/api.product-reviews.ts
@@ -6,17 +6,43 @@ import { upsertProductReviews } from '@libs/util/server/data/product-reviews.ser
 import { StoreUpsertProductReviewsDTO } from '@lambdacurry/medusa-plugins-sdk';
 import { upsertProductReviewsValidator } from '@app/components/reviews/product-form-validators';
 import { sdk } from '@libs/util/server/client.server';
+// import { File, Blob, Buffer } from 'buffer';
+import { createReadStream, ReadStream } from 'fs';
 
 export enum ProductReviewAction {
   UPSERT_PRODUCT_REVIEWS = 'upsertProductReviews',
 }
+// const nodeOnDiskFileToFile = async (nodeFile: NodeOnDiskFile): Promise<File> => {
+//   const storedFile = await readFile(nodeFile.getFilePath());
+//   console.log('🚀 ~ nodeOnDiskFileToFile ~ storedFile:', storedFile);
+
+//   return new File([storedFile], nodeFile.name, {
+//     type: nodeFile.type,
+//     lastModified: nodeFile.lastModified,
+//   });
+// };
 
 const uploadImages = async (images: NodeOnDiskFile | NodeOnDiskFile[] | null | undefined): Promise<string[]> => {
+  console.log('🚀 ~ uploadImages ~ images:', images);
   if (!images) return [];
   if (images && !Array.isArray(images)) images = [images];
   if (!Array.isArray(images)) return [];
   if (!images?.length) return [];
-  const response = await sdk.store.productReviews.uploadImages(images as unknown as File[]);
+  // console.log('🚀 ~ uploadImages ~ images:', images[0].);
+  // const files = images
+  //   .filter((image) => image.getFilePath)
+  //   .map((image) => {
+  //     return {
+  //       filePath: image.getFilePath(),
+  //       name: image.name,
+  //     };
+  //   });
+  const filesPaths = await Promise.all(images.map(async (image) => image.getFilePath()));
+  console.log('🚀 ~ uploadImages ~ filesPaths:', filesPaths);
+  const readStreams: ReadStream[] = filesPaths.map((filePath) => createReadStream(filePath));
+
+  const response = await sdk.store.productReviews.uploadImages(readStreams);
+  console.log('🚀 ~ uploadImages ~ response:', response);
   if (!response?.uploads) return [];
   return Array.isArray(response.uploads) ? response.uploads.map((i) => i.url) : [response.uploads.url];
 };
