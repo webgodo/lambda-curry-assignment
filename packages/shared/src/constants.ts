df31a977a968503d03b2a8eb0c5424e2a394cd9d
Mohsen Ghaemaghami <m.ghaemmaghami@bettermode.com>
2025-04-21 12:20:17 +0330

    feat: add @lc/shared package with constants defined

---
+++ packages/shared/src/constants.ts
@@ -0,0 +1,8 @@
+export const PRODUCT_METADATA_KEYS = {
+    IS_CUSTOMIZABLE: "is_customizable",
+  };
+  
+  export const LINE_ITEM_METADATA_KEYS = {
+    CUSTOM_MESSAGE: "custom_message",
+  };
+  
