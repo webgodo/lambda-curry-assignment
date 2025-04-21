b9e4b3a10c2f57a61d6ab932c6d4073d88d94fc6
Mohsen Ghaemaghami <m.ghaemmaghami@bettermode.com>
2025-04-21 14:59:31 +0330

    feat: add Toggle button to control customizing the metadata in the admin

---
+++ apps/medusa/src/admin/widgets/product-customizable.tsx
@@ -0,0 +1,58 @@
+import { defineWidgetConfig } from "@medusajs/admin-sdk";
+import { Button, Container, Heading, Label, clx } from "@medusajs/ui";
+import { PRODUCT_METADATA_KEYS } from "@lc/shared";
+import { DetailWidgetProps, AdminProduct } from "@medusajs/framework/types";
+import { useMutation } from "@tanstack/react-query";
+import { sdk } from "../../sdk";
+
+const ProductCustomizable = ({
+  data: product,
+}: DetailWidgetProps<AdminProduct>) => {
+  const productId = product.id;
+
+  // Determine if the product is customizable from metadata
+  const isCustomizable =
+    !!product.metadata[PRODUCT_METADATA_KEYS.IS_CUSTOMIZABLE];
+
+  const { mutateAsync: updateProduct, isPending } = useMutation({
+    mutationFn: (newValue: boolean) =>
+      sdk.admin.product.update(productId, {
+        metadata: {
+          ...product.metadata,
+          [PRODUCT_METADATA_KEYS.IS_CUSTOMIZABLE]: newValue ? true : undefined,
+        },
+      }),
+    onSuccess() {
+      window.location.reload();
+    },
+  });
+
+  const handleButtonClick = async () => await updateProduct(!isCustomizable);
+
+  return (
+    <Container className="flex flex-col gap-4">
+      <Heading level="h2">Customizable Product</Heading>
+
+      <div
+        className={clx("flex items-center gap-x-2", {
+          "animate-pulse": isPending,
+        })}
+      >
+        <Button
+          onClick={handleButtonClick}
+          disabled={isPending} // Disable button while updating
+          variant={isCustomizable ? "primary" : "secondary"}
+        >
+          {isCustomizable ? "Disable custom message" : "Enable custom message"}
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+// The widget's configurations
+export const config = defineWidgetConfig({
+  zone: "product.details.side.after",
+});
+
+export default ProductCustomizable;
---
+++ apps/medusa/src/sdk.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/types/importMeta.d.ts" />
+import Medusa from "@medusajs/js-sdk"
+
+export const sdk = new Medusa({
+    baseUrl: import.meta.env.ADMIN_BACKEND_URL || "/",
+    debug: import.meta.env.NODE_ENV === 'development',
+    auth: {
+        type: "session",
+    },
+})
